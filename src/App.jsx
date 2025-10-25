079fa7129d362fc8dd918e29dfdf0eea8325690d
TheSaviourEking <saviour.eking@cyberservices.com>
2025-10-25 09:37:05 +0100

    Add Hero component with GSAP animations for Mojito Cocktails app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { ScrollTrigger, SplitText } from "gsap/all";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <div className="h-dvh"></div>
     </main>
   );
 }
